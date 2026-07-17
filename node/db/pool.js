ac03c20d526e2f92babfdf2e936a1717b9dc07fa
JONE 500 <Ingkawat2023Reals@gmail.com>
2026-07-18 02:47:28 +0700

    Guy....

--- node/db/pool.js
+++ node/db/pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "justthisuser",
-  host: "db",
-  database: "my_database",
-  password: "mysqlpass1122",
-  port: 5432,
+  user: process.env.DB_USER || "justthisuser",
+  host: process.env.DB_HOST || "db",
+  database: process.env.DB_NAME || "my_database",
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
